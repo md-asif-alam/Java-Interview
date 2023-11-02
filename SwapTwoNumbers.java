627708dc426958a53d28796b85d51402367b576f
Md Asif Alam <mdasifalam910@gmail.com>
2023-11-02 12:19:01 +0530

    addition and subtraction method

--- SwapTwoNumbers.java
+++ SwapTwoNumbers.java
@@ -3,10 +3,16 @@ public class SwapTwoNumbers {
         int a=10;
         int b=20;
         System.out.println("Before Swapping values are "+a+" and "+b);
+//      Using third variable
 
-        int temp=a;
-        a=b;
-        b=temp;
+//        int temp=a;
+//        a=b;
+//        b=temp;
+
+        //addition and subtraction
+        a=a+b;
+        b=a-b;
+        a=a-b;
         System.out.println("AfterSwapping values are "+a+" and "+b);
     }
 }
