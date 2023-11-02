da2291c89c778f2b8564fe4669803a7f0572f9f1
Md Asif Alam <mdasifalam910@gmail.com>
2023-11-02 12:50:46 +0530

    StringBuffer Class

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -3,13 +3,18 @@ public class ReverseNumber {
         int num=1234;
 
         //1. using algorithm
-        int temp=num;
-        int rev=0;
-        while(num!=0){
-            rev=rev*10 + num%10;
-            num=num/10;
-        }
-        System.out.println("Reverse of "+temp+" is "+rev);
+//        int temp=num;
+//        int rev=0;
+//        while(num!=0){
+//            rev=rev*10 + num%10;
+//            num=num/10;
+//        }
+
+        //2.Using StringBuffer class
+        StringBuffer rev;
+        StringBuffer sb=new StringBuffer(String.valueOf(num));
+        rev=sb.reverse();
+        System.out.println("Reverse of "+num+" is "+rev);
 
     }
 }
